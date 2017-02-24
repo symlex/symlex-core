af132b145f329fe25e5a7a816dcaf913e0ebed1e
Michael Mayer <michael@liquidbytes.net>
2017-02-24 13:32:33 +0100

    Added tests for twig and rest router

--- src/Tests/Router/RestRouterTest.php
+++ src/Tests/Router/RestRouterTest.php
@@ -2,6 +2,8 @@
 
 namespace Symlex\Tests\Router;
 
+use Silex\Application;
+use Symfony\Component\HttpFoundation\Request;
 use TestTools\TestCase\UnitTestCase;
 use Symlex\Router\RestRouter;
 
@@ -16,14 +18,74 @@ class RestRouterTest extends UnitTestCase
      */
     protected $router;
 
+    /**
+     * @var Application
+     */
+    protected $app;
+
+    /**
+     * @var FakeRestController
+     */
+    protected $controller;
+
+    /**
+     * @var Container
+     */
+    protected $container;
+
     public function setUp()
     {
-        $this->router = $this->get('router.rest');
+        $this->container = $this->getContainer();
+        $this->app = $this->container->get('app');
+        $this->router = $this->container->get('router.rest');
+        $this->controller = $this->container->get('controller.rest.fake');
     }
 
-    public function testRoute()
+    public function testCgetRoute()
     {
-        $this->router->route();
-        $this->assertTrue(true);
+        $request = Request::create('http://localhost/api/fake');
+        $this->router->route('/api', 'controller.rest.');
+        $response = $this->app->handle($request);
+        $result = json_decode($response->getContent(), true);
+        $this->assertEquals('cgetAction', $this->controller->actionName);
+        $this->assertInstanceOf(Request::class, $this->controller->request);
+        $this->assertInternalType('array', $result);
+        $this->assertArrayHasKey('request', $result);
+        $this->assertArrayHasKey('actionName', $result);
+        $this->assertEquals('cgetAction', $result['actionName']);
+        $this->assertInternalType('array', $result['request']);
+    }
+
+    public function testGetRoute()
+    {
+        $request = Request::create('http://localhost/api/fake/345');
+        $this->router->route('/api', 'controller.rest.');
+        $response = $this->app->handle($request);
+        $result = json_decode($response->getContent(), true);
+        $this->assertEquals('getAction', $this->controller->actionName);
+        $this->assertInstanceOf(Request::class, $this->controller->request);
+        $this->assertInternalType('array', $result);
+        $this->assertArrayHasKey('request', $result);
+        $this->assertArrayHasKey('actionName', $result);
+        $this->assertEquals('getAction', $result['actionName']);
+        $this->assertEquals(345, $result['id']);
+        $this->assertInternalType('array', $result['request']);
+    }
+
+    public function testOptionsCommentRoute()
+    {
+        $request = Request::create('http://localhost/api/fake/345/comment/1', 'OPTIONS');
+        $this->router->route('/api', 'controller.rest.');
+        $response = $this->app->handle($request);
+        $result = json_decode($response->getContent(), true);
+        $this->assertEquals('optionsCommentAction', $this->controller->actionName);
+        $this->assertInstanceOf(Request::class, $this->controller->request);
+        $this->assertInternalType('array', $result);
+        $this->assertArrayHasKey('request', $result);
+        $this->assertArrayHasKey('actionName', $result);
+        $this->assertEquals('optionsCommentAction', $result['actionName']);
+        $this->assertEquals(345, $result['id']);
+        $this->assertEquals(1, $result['commentId']);
+        $this->assertInternalType('array', $result['request']);
     }
 }
--- src/Tests/Router/TwigRouterTest.php
+++ src/Tests/Router/TwigRouterTest.php
@@ -2,6 +2,8 @@
 
 namespace Symlex\Tests\Router;
 
+use Silex\Application;
+use Symfony\Component\HttpFoundation\Request;
 use TestTools\TestCase\UnitTestCase;
 use Symlex\Router\TwigRouter;
 
@@ -16,14 +18,56 @@ class TwigRouterTest extends UnitTestCase
      */
     protected $router;
 
+    /**
+     * @var Application
+     */
+    protected $app;
+
+    /**
+     * @var FakeWebController
+     */
+    protected $controller;
+
+    /**
+     * @var Container
+     */
+    protected $container;
+
     public function setUp()
     {
-        $this->router = $this->get('router.twig');
+        $this->container = $this->getContainer();
+        $this->app = $this->container->get('app');
+        $this->router = $this->container->get('router.twig');
+        $this->controller = $this->container->get('controller.web.fake');
     }
 
-    public function testRoute()
+    public function testIndexRoute()
     {
-        $this->router->route();
-        $this->assertTrue(true);
+        $request = Request::create('http://localhost/fake/index');
+        $this->router->route('/', 'controller.web.');
+        $response = $this->app->handle($request);
+        $this->assertEquals('indexAction', $response->getContent());
+        $this->assertEquals('indexAction', $this->controller->actionName);
+        $this->assertInstanceOf(Request::class, $this->controller->request);
+    }
+
+    public function testPostIndexRoute()
+    {
+        $request = Request::create('http://localhost/fake/index', 'POST');
+        $this->router->route('/', 'controller.web.');
+        $response = $this->app->handle($request);
+        $this->assertEquals('postIndexAction', $response->getContent());
+        $this->assertEquals('postIndexAction', $this->controller->actionName);
+        $this->assertInstanceOf(Request::class, $this->controller->request);
+    }
+
+    public function testFooRoute()
+    {
+        $request = Request::create('http://localhost/fake/foo/345', 'GET');
+        $this->router->route('/', 'controller.web.');
+        $response = $this->app->handle($request);
+        $this->assertEquals('fooAction', $response->getContent());
+        $this->assertEquals('fooAction', $this->controller->actionName);
+        $this->assertInstanceOf(Request::class, $this->controller->request);
     }
 }
